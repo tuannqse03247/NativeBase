68d310ed2498278871b5bb958a2001aa0e857e7c
Nguyen Quoc Tuan <tuannqse03247@tuannqse03247.local>
2018-07-11 15:57:20 +0700

    Fix Icomoon not found

--- src/basic/CustomFont/MyIcons.js
+++ src/basic/CustomFont/MyIcons.js
@@ -1,4 +1,4 @@
 import { createIconSetFromIcoMoon } from 'react-native-vector-icons';
 import icoMoonConfig from './selection.json';
-const MyIcons = createIconSetFromIcoMoon(icoMoonConfig, 'icomoon', 'MyIcons.ttf');
+const MyIcons = createIconSetFromIcoMoon(icoMoonConfig, 'MyIcons', 'MyIcons.ttf');
 export { MyIcons }
